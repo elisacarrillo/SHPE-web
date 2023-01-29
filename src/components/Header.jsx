9ab8aed54d2eae226d5311bf4b4b8f1b168336b6
elisa <elisamc2@illinois.edu>
2023-01-29 00:08:48 -0600

    added logout functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,32 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-
+import { auth } from '../base';
+import './Header.css'
+import { useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 function Header() {
+    const [signedIn, setSignedIn] = useState(false);
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+          // User is signed in, see docs for a list of available properties
+          // https://firebase.google.com/docs/reference/js/firebase.User
+          const uid = user.uid;
+          setSignedIn(true);
+
+          
+          // ...
+        } else {
+          // User is signed out
+        //   navigate('/login');
+        //   window.location.reload();
+          // ...
+        }
+      });
+       const logout = () => {
+            auth.signOut();
+            window.location.reload("/signin")
+        };
+ 
     return (
         <Navbar className="color-nav" expand="lg" sticky='top'>
 
@@ -15,7 +40,19 @@ function Header() {
             <Nav.Link href="/">Calendar</Nav.Link>
             <Nav.Link href="/">Contact Us</Nav.Link>
             </Nav>
-            <Nav.Link href="/login">Login / Sign Up</Nav.Link>
+            <div>
+                {
+                    signedIn
+                    ? (
+                        <Nav.Link onClick={logout}>Logout</Nav.Link>
+                    )
+                    :
+                    (
+                        <Nav.Link href="/login">Login / Sign Up</Nav.Link>
+                    )
+                }
+            </div>
+            
         </Navbar.Collapse>
         </Navbar>
     );
