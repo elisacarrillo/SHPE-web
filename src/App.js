a1e081f30f764df2eee10162290afad811b9bd7e
elisa <elisamc2@illinois.edu>
2023-04-08 15:27:22 -0500

    Updated styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
+import Profile from './components/ProfilePage';
+import Header from './components/Header';
 
 function App() {
   return (
+    <>
+    < Header />
     <Router>
+      
       <Routes>
         < Route path="/" element={<Home />} />
         < Route path='/login' element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<h1>404 Not Found</h1>} />
+
       </Routes>
     </Router>
+    </>
   );
 }
 
